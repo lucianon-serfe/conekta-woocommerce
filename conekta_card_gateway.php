bec9aa9b5d01c52b1ba02cff4ce20a262b8d9cfd
lucianon <lucianon@serfe.com>
2021-03-01 10:43:22 -0300

    0082448: [T][WooCommerce] Codacy check

--- conekta_card_gateway.php
+++ conekta_card_gateway.php
@@ -159,13 +159,13 @@ class WC_Conekta_Card_Gateway extends WC_Conekta_Plugin
         if (!$this->use_sandbox_api
           && get_option('woocommerce_force_ssl_checkout') == 'no'
           && $this->enabled == 'yes') {
-            echo( '<div class="error"><p>'
+            return( '<div class="error"><p>'
               .sprintf(
-                __('%s sandbox testing is disabled and can performe live transactions'
+                _e($this->GATEWAY_NAME .' sandbox testing is disabled and can performe live transactions'
                 .' but the <a href="%s">force SSL option</a> is disabled; your checkout'
                 .' is not secure! Please enable SSL and ensure your server has a valid SSL'
                 .' certificate.', 'woothemes'),
-                $this->GATEWAY_NAME, admin_url('admin.php?page=settings')
+                admin_url('admin.php?page=settings')
               )
             .'</p></div>');
         }
